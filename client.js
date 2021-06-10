d14cbefc1ea9b5c7aff91793d63f283cadc02452
Kade Zimmerman <kaderz0021@gmail.com>
2021-06-10 16:29:27 -0600

    final commit

--- client.js
+++ client.js
@@ -8,8 +8,12 @@ const client = net.createConnection({port: 5000}, () => {
     process.stdin.pipe(client);
     
     client.on('data', (data) => {
-        console.log(data);
-    });
+        if(data.toString() == `\nYou have been kicked out of the server. \n`) {
+            console.log(data.toString());
+            client.end();
+        } 
+        else console.log(data.toString())
+    })
     client.on('close', () => {
         console.log('Disconnected from server');
     });
